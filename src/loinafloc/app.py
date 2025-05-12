0d6d08ac5667f8da0158a4323fffa2691ccc3562
Tom Althaus <berstheim67@gmail.com>
2025-05-12 23:28:22 +0200

    Début création menu

--- src/loinafloc/app.py
+++ src/loinafloc/app.py
@@ -2,13 +2,14 @@
 Cette application vous permet de créer vos courses d'orientations sur une carte du monde
 """
 
-import toga
+import toga, sys, asyncio
 from toga.style import Pack
 from toga.style.pack import COLUMN, ROW
 from toga.widgets.box import Box
 from toga.window import MainWindow
 from toga.app import App
 from toga.widgets.mapview import *
+from toga.dialogs import InfoDialog
 
 class Globalorientation(App):
     def startup(self):
@@ -18,16 +19,31 @@ class Globalorientation(App):
         We then create a main window (with a name matching the app), and
         show the main window.
         """
-        main_box = Box()
+        self.main_box = Box()
 
         self.main_window = MainWindow(title=self.formal_name)
-        self.main_window.content = main_box
-
-        de_gaulle = MapPin(location=[48.762281, 7.689851], title="Wahlenheim de Gaulle", subtitle="Un super arrêt de bus")
-        map_view = MapView(location=[48.762659, 7.688413], style=Pack(flex=1), zoom=18)
-        map_view.pins.add(de_gaulle)
-        main_box.add(map_view)
+        self.main_window.content = self.main_box
         self.main_window.show()
+
+        asyncio.create_task(self.main())
+
+    async def main(self):
+        if self.location.has_permission:
+            #location_state = await self.location.request_permission()
+            localisation = await self.location.current_location()
+            print(localisation[0], localisation[1])
+            map_view = MapView(location=localisation, style=Pack(flex=1), zoom=4)
+            location_pins = MapPin(location=localisation, title="Vous êtes ici")
+            map_view.pins.add(location_pins)
+        else:
+            map_view = MapView(location=[46.740948, 2.535976], style=Pack(flex=1), zoom=4)
+        self.main_box.add(map_view)
+
+        if not self.location.has_permission:
+            InfoDialog("Permission requise", "Nous avons besoins d'accéder à votre position pour que l'application fonctionne sans problème!")
+            location_state = await self.location.request_permission()
+            if location_state == False:
+                sys.exit()
 
 
 def main():
